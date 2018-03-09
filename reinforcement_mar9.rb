ee8ee10a5cc0be981d088247a590bd67b89eee58
debbie <drosenfeld87@gmail.com>
2018-03-09 17:55:27 -0500

    still working on loop

--- reinforcement_mar9.rb
+++ reinforcement_mar9.rb
@@ -1,1 +1,11 @@
 a = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+
+a.each do |string|
+  if a = string.downcase
+    puts "lowercase"
+  elsif a = string.length == 5
+    puts "long"
+  else
+    puts string
+  end
+ end
