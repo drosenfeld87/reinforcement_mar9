b491a4926c9f1db0c7b94a0ae9d4ca7d672ce80e
debbie <drosenfeld87@gmail.com>
2018-03-11 10:59:51 -0400

    complete exercise

--- reinforcement_mar9.rb
+++ reinforcement_mar9.rb
@@ -1,10 +1,12 @@
 a = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
 
 a.each do |string|
-  if a = string.downcase
-    puts "lowercase"
-  elsif a = string.length == 5
+  if string.length > 4
     puts "long"
+  elsif string.length > 4 && string.downcase
+    puts "long and lowercase"
+  elsif string == string.downcase
+    puts "lowercase"
   else
     puts string
   end
